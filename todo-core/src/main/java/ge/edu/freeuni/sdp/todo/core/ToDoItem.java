5045a96bf48dc95ab7b13d5b69c451514c45574f
gmamaladze <gmamaladze@gmail.com>
2015-04-10 18:58:34 +0200

    Forgotten file

---
+++ todo-core/src/main/java/ge/edu/freeuni/sdp/todo/core/ToDoItem.java
@@ -0,0 +1,14 @@
+package ge.edu.freeuni.sdp.todo.core;
+
+public class ToDoItem {
+	private String text;
+	
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
